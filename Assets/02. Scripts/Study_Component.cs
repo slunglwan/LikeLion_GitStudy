7e3a38e23545f5456fdd21f8ead0ff5f424dc0be
Clonix <syghwan@gmail.com>
2025-05-22 09:24:40 +0900

    [ADD} annotation
    -- Component에 주석 추가

--- Assets/02. Scripts/Study_Component.cs
+++ Assets/02. Scripts/Study_Component.cs
@@ -1,14 +1,18 @@
-using UnityEngine;
+using UnityEngine;// UnityEngine이라는 namespace를 사용한다는 선언
 
 public class Study_Component : MonoBehaviour
+//접근제한자 클래스명
 {
     public GameObject obj;
+  //접근제한자 데이터타입 변수이름
+    
     public string changeName;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    //타입 함수명
     {
         obj = GameObject.Find("Main Camera");
-
+        //     타입      찾겠다.
         obj.name = changeName;
     }
 }
